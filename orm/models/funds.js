9662df88db3ff497442b5867b17640bc5b91c879
IljaziEzan <ezan.iljazi@drweb.mk>
2022-05-26 14:26:19 +0200

    fixed relation of funds

--- orm/models/funds.js
+++ orm/models/funds.js
@@ -7,8 +7,8 @@ module.exports = (sequelize, DataTypes) => {
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-    static associate(model) {
-      Funds.belongsTo(model.pools)
+    static associate() {
+    // Relations are here
     }
   }
   Funds.init(
